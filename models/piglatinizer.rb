ccbb13fa14fd40d9c9f629f0a34c69fc8e4ef1bc
Anthony Pardo <pardo.anthony@gmail.com>
2020-03-20 20:58:55 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,17 +1,20 @@
 class PigLatinizer 
-  def piglatinize(words)
+  def piglatinize(str)
+    words = str.split(' ')
+    
     alpha = ('a'..'z').to_a
     vowels = %w[a e i o u]
     consonants = alpha - vowels
   
-    if vowels.include?(str[0])
-      str + 'ay'
-    elsif consonants.include?(str[0]) && consonants.include?(str[1])
-      str[2..-1] + str[0..1] + 'ay'
-    elsif consonants.include?(str[0])
-      str[1..-1] + str[0] + 'ay'
-    else
-      str # return unchanged
-    end
+    words.each do |word| 
+      if vowels.include?(str[0])
+        str + 'ay'
+      elsif consonants.include?(str[0]) && consonants.include?(str[1])
+        str[2..-1] + str[0..1] + 'ay'
+      elsif consonants.include?(str[0])
+        str[1..-1] + str[0] + 'ay'
+      else
+        str # return unchanged
+      end
   end
 end
